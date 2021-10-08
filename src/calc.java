bcc7f8420b866706083a5b50114f661520e09898
MatthiasSchober <52110387@edu.campus02.at>
2021-10-08 20:22:40 +0200

    Rechenoperationen erstellt

---
+++ src/calc.java
@@ -0,0 +1,15 @@
+public class calc {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        a =5;
+        b= 6;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+
+
+    }
+}
--- src/calculator.java
+++ src/calculator.java
@@ -1,14 +1,19 @@
 public class calculator {
     public static void main(String[] args) {
-        int kg = 10;
+        int kg = 9;
         System.out.println("kg = " + kg);
-        double temp = 30;
+        double temp = 988;
+
         System.out.println("temp = " + temp);
         boolean trinken = true;
         System.out.println("trinken = " + trinken);
         char tee = 't';
         String gedicht = "Hello Word";
         System.out.println("gedicht = " + gedicht);
+
+        //modul
+
+
 
 
     }
