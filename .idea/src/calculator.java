3e2b2f1e9ca9f87cd39bd5821faca5c84906bfdb
MatthiasSchober <52110387@edu.campus02.at>
2021-10-08 19:59:06 +0200

    Variablen bestimmt

--- .idea/src/calculator.java
+++ .idea/src/calculator.java
@@ -1,5 +1,15 @@
 public class calculator {
     public static void main(String[] args) {
+        int kg = 10;
+        System.out.println("kg = " + kg);
+        double temp = 30;
+        System.out.println("temp = " + temp);
+        boolean trinken = true;
+        System.out.println("trinken = " + trinken);
+        char tee = 't';
+        String gedicht = "Hello Word";
+        System.out.println("gedicht = " + gedicht);
+
 
     }
 }
